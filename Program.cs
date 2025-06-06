e866c7ba72bac694053759954736f4f65ea14cc2
Kloshar <kloshar13@yahoo.com>
2025-06-06 17:38:52 +0300

    fgfgfg

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 {
     static void Main()
     {
+        string scoreString = Console.ReadLine();
+        string namesString = Console.ReadLine();
+
 
         Console.WriteLine($"Hello!");
         Console.ReadKey();
