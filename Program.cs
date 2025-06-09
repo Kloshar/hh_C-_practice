07ca4e5a9cf707da66dd0a0ebe2d87f7589c3fd5
Kloshar <kloshar13@yahoo.com>
2025-06-09 17:34:05 +0300

    Raiting begining

--- Program.cs
+++ Program.cs
@@ -1,30 +1,90 @@
-﻿class Program
+﻿using System.Diagnostics.CodeAnalysis;
+
+class Program
 {
     static void Main()
     {
         //Exam();
         //Geometry();
         //SymbolsHiding();
-
-        Survey();
+        //Survey();
+        Raiting();
         Console.ReadKey();
     }
-    static void Survey()
+    static void Raiting()
     {
-        //string inpupString = Console.ReadLine();
-        string inpupString = "132189";
-        int result = DigitalRoot(int.Parse(inpupString));
+        Dictionary<string, int[]> candidates = GetCandidates();
 
+        //ICollection<string> keys = candidates.Keys;
+        //foreach (string candidate in keys) Console.WriteLine($"{candidate}, {candidates[candidate].Length}");
 
-        static int DigitalRoot(int n)
+        foreach (string candidate in SelectedCandidates(candidates)) Console.WriteLine(candidate);
+         
+        static Dictionary<string, int[]> GetCandidates()
         {
-            
+            Dictionary<string, int[]> dict = new Dictionary<string, int[]>();
+            string candidate;
+
+            //while ((candidate = Console.ReadLine()) != null && candidate != "")
+            //{
+            //    var parts = candidate.Split(',');
+            //    string name = parts[0];
+            //    var scores = parts.Skip(1).Select(int.Parse).ToArray();
+            //    dict[name] = scores;
+            //}
+
+            dict.Add("Ivanov", new int[] { 5, 6, 8, 5 });
+            dict.Add("Lisii", new int[] { 9, 8, 10, 9 });
+            dict.Add("Sokolova", new int[] { 5, 6, 8, 5 });
+            dict.Add("Tritonov", new int[] { 7, 2, 3, 4 });
+            dict.Add("Chernov", new int[] { 8, 8, 8, 8 });
+            dict.Add("Svetova", new int[] { 4, 5, 3, 6 });
+            dict.Add("Zayatz", new int[] { 5, 6, 8, 5 });
+            dict.Add("Rezhik", new int[] { 6, 6, 8 });
+
+            return dict;
+        }
+        static List<string> SelectedCandidates(Dictionary<string, int[]> candidates)
+        {
+            List<string> result = new List<string>();
+
+            List<Human> cands = new List<Human>();           
+
+            ICollection<string> keys = candidates.Keys;
+            foreach (string candidate in keys)
+            {
+                int[] numbers = candidates[candidate];
+                double average = Math.Round(numbers.Average(), 1);
+                cands.Add(new Human(candidate, average));
+                //Console.WriteLine($"Имя: {candidate}, Средний балл: {average}");
+            }
+
+            cands.Sort();
+
+            foreach (Human h in cands)
+            {
+                Console.WriteLine($"{h.Name}, {h.Rating:F2}", System.Globalization.CultureInfo.GetCultureInfo("en-US"));
+            }
+            return result;
+        }
 
 
-            return 1;
+    }
+    class Human : IComparable
+    {
+        public string Name {  get; set; }
+        public double Rating { get; set; }
+        public Human(string name, double rating)
+        {
+            Name = name;
+            Rating = rating;
+        }
+        public int CompareTo(object obj)
+        {
+            Human human = obj as Human;
+            return Rating.CompareTo(human.Rating);
         }
     }
-
     static void Exam()
     {
         //string scoreString = Console.ReadLine();
@@ -100,4 +160,29 @@
             return newString;
         }
     }
+    static void Survey()
+    {
+        //string inpupString = Console.ReadLine();
+        string inpupString = "3";
+        int result = DigitalRoot(int.Parse(inpupString));
+        Console.WriteLine(result);
+
+        static int DigitalRoot(int n)
+        {
+            int result = n;
+            int vremResult = 0;
+            do
+            {
+                vremResult = 0;
+                for (int i = 0; i < result.ToString().Length; i++) // result = 18
+                {
+                    vremResult += Convert.ToInt32(result.ToString()[i].ToString());
+                }
+                result = vremResult;
+
+            } while (vremResult >= 10);
+            return result;
+        }
+    }
+
 }
