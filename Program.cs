0120fc96895aad301597819b3f7e4f098a69fae0
Kloshar <kloshar13@yahoo.com>
2025-08-22 10:52:00 +0300

    ServerAnalyzer finished

--- Program.cs
+++ Program.cs
@@ -43,87 +43,95 @@ class Program
          до тех пор, пока вводится пустая строка
          */
 
-        List<string> input = new List<string>()
-        {
-            " <service=\"31007\" data=\"ABCDEFGHI\" action=\"write\">",
-            "<service=\"32008\" data=\"JKLMNOPQR\" action=\"read\"> ",
-            "<service=\"31007\" data=\"STUVWXYZA\" action=\"write\">", 
-            "<service=\"32008\" data=\"BCDEFGHIJ\" action=\"read\">",
-            "<service=\"31007\" data=\"KLMNOPQRS\" action=\"write\">",
-            "<service=\"32008\" data=\"TUVWXYZAB\" action=\"read\">", 
-            "<service=\"31007\" data=\"CDEFGHIJK\" action=\"write\">", 
-            "<service=\"32008\" data=\"LMNOPQRST\" action=\"read\">"
-        };
+        List<string> input = new List<string>();
 
-        string str = Console.ReadLine();
+        string str;
+        do
+        {
+            str = Console.ReadLine();
+            input.Add(str);
 
+        } while (str != "");
 
-        input = new List<string>()
-        {
-            "<service=\"41017\" data=\"ABCDEFGHI\" action=\"write\">",
-            "<service=\"42018\" data=\"UKLMNOPQR\" action=\"read\"> ", 
-            "<service=\"41017\" data=\"STUVWXYZA\" action=\"write\">", 
-            "<service=\"42018\" data=\"BCDEFGHIJ\" action=\"read\">", 
-            "<service=\"41017\" data=\"KLMNOPQRS\" action=\"write\">", 
-            "<service=\"42018\" data=\"TUVWXYZAB\" action=\"read\">", 
-            "<service=\"41017\" data=\"CDEFGHIJK\" action=\"write\">", 
-            "<service=\"42018\" data=\"LMNOPQRST\" action=\"read\"> ", 
-            "<service=\"41017\" data=\"UVWXYZABC\" action=\"write\">", 
-            "<service=\"42018\" data=\"XYZABCDEF\" action=\"read\">", 
-            "<service=\"41017\" data=\"YZABCDEFG\" action=\"write\">", 
-            "<service=\"42018\" data=\"ZABCDEFGH\" action=\"read\">", 
-            "<service=\"41017\" data=\"NEWDATAID\" action=\"write\">", 
-            "<service=\"42018\" data=\"ANOTHERID\" action=\"write\">"
-        };
-        input = new List<string>()
-        {
-            "<service=\"29005\" data=\"ABCDEFGHI\" action=\"read\">", 
-            "<service=\"30006\" data=\"JKLMNOPQR\" action=\"write\">", 
-            "<service=\"29005\" data=\"STUVWXYZA\" action=\"read\">", 
-            "<service=\"30006\" data=\"BCDEFGHIJ\" action=\"write\">", 
-            "<service=\"29005\" data=\"KLMNOPQRS\" action=\"read\">", 
-            "<service=\"30006\" data=\"TUVWXYZAB\" action=\"write\">", 
-            "<service=\"29005\" data=\"CDEFGHIJK\" action=\"read\">", 
-            "<service=\"30006\" data=\"LMNOPQRST\" action=\"write\">"
-        };
-        input = new List<string>()
-        {
-            "<service=\"34010\" data=\"ABCDEFGHI\" action=\"write\">", 
-            "<service=\"34010\" data=\"JKLMNOPQR\" action=\"write\">", 
-            "<service=\"34010\" data=''STUVWXYZA\" action=\"write\">", 
-            "<service=\"34010\" data=\"BCDEFGHIJ\" action=\"write\">", 
-            "<service=\"34010\" data=\"KLMNOPQRS\" action=\"write\">", 
-            "<service=\"34010\" data=\"TUVWXYZAB\" action=”read\">", 
-            "<service=\"34010\" data=\"CDEFGHIJK\" action=\"write\">", 
-            "<service=\"34010\" data=\"LMNOPQRST\" action=\"write\">", 
-            "<service=\"34010\" data=\"UVWXYZABC\" action=\"write\">", 
-            "<service=\"34010\" data-'XYZABCDEF\" action=\"read\">", 
-            "<service=\"34010\" data=\"YZABCDEFG\" action=\"write\">", 
-            "<service=\"34010\" data=\"ZABCDEFGH\" action=\"write\">"
-        };
-        input = new List<string>()
-        {
-            "<service=\"41017\" data=\"ABCDEFGHI\" action=\"write\">",
-            "<service=\"42018\" data=\"UKLMNOPQR\" action=\"read\"> ",
-            "<service=\"41017\" data=\"STUVWXYZA\" action=\"write\">",
-            "<service=\"42018\" data=\"BCDEFGHIJ\" action=\"read\">",
-            "<service=\"41017\" data=\"KLMNOPQRS\" action=\"write\">",
-            "<service=\"42018\" data=\"TUVWXYZAB\" action=\"read\">",
-            "<service=\"41017\" data=\"CDEFGHIJK\" action=\"write\">",
-            "<service=\"42018\" data=\"LMNOPQRST\" action=\"read\"> ",
-            "<service=\"41017\" data=\"UVWXYZABC\" action=\"write\">",
-            "<service=\"42018\" data=\"XYZABCDEF\" action=\"read\">",
-            "<service=\"41017\" data=\"YZABCDEFG\" action=\"write\">",
-            "<service=\"42018\" data=\"ZABCDEFGH\" action=\"read\">",
-            "<service=\"41017\" data=\"NEWDATAID\" action=\"write\">",
-            "<service=\"41019\" data=\"NEWDATAID\" action=\"write\">",
-            "<service=\"41020\" data=\"NEWDATAID\" action=\"write\">",
-            "<service=\"41021\" data=\"NEWDATAID\" action=\"write\">",
-            "<service=\"41022\" data=\"NEWDATAID\" action=\"write\">",
-            "<service=\"42018\" data=\"ANOTHERID\" action=\"write\">",
-            "<service=\"41019\" data=\"NEWDATAID\" action=\"read\">",
-            "<service=\"41019\" data=\"NEWDATAID\" action=\"read\">"
-        };
+        foreach (string s in input) Console.WriteLine(s);
+
+        //input = new List<string>()
+        //{
+        //    " <service=\"31007\" data=\"ABCDEFGHI\" action=\"write\">",
+        //    "<service=\"32008\" data=\"JKLMNOPQR\" action=\"read\"> ",
+        //    "<service=\"31007\" data=\"STUVWXYZA\" action=\"write\">", 
+        //    "<service=\"32008\" data=\"BCDEFGHIJ\" action=\"read\">",
+        //    "<service=\"31007\" data=\"KLMNOPQRS\" action=\"write\">",
+        //    "<service=\"32008\" data=\"TUVWXYZAB\" action=\"read\">", 
+        //    "<service=\"31007\" data=\"CDEFGHIJK\" action=\"write\">", 
+        //    "<service=\"32008\" data=\"LMNOPQRST\" action=\"read\">"
+        //};
+        //input = new List<string>()
+        //{
+        //    "<service=\"41017\" data=\"ABCDEFGHI\" action=\"write\">",
+        //    "<service=\"42018\" data=\"UKLMNOPQR\" action=\"read\"> ", 
+        //    "<service=\"41017\" data=\"STUVWXYZA\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"BCDEFGHIJ\" action=\"read\">", 
+        //    "<service=\"41017\" data=\"KLMNOPQRS\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"TUVWXYZAB\" action=\"read\">", 
+        //    "<service=\"41017\" data=\"CDEFGHIJK\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"LMNOPQRST\" action=\"read\"> ", 
+        //    "<service=\"41017\" data=\"UVWXYZABC\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"XYZABCDEF\" action=\"read\">", 
+        //    "<service=\"41017\" data=\"YZABCDEFG\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"ZABCDEFGH\" action=\"read\">", 
+        //    "<service=\"41017\" data=\"NEWDATAID\" action=\"write\">", 
+        //    "<service=\"42018\" data=\"ANOTHERID\" action=\"write\">"
+        //};
+        //input = new List<string>()
+        //{
+        //    "<service=\"29005\" data=\"ABCDEFGHI\" action=\"read\">", 
+        //    "<service=\"30006\" data=\"JKLMNOPQR\" action=\"write\">", 
+        //    "<service=\"29005\" data=\"STUVWXYZA\" action=\"read\">", 
+        //    "<service=\"30006\" data=\"BCDEFGHIJ\" action=\"write\">", 
+        //    "<service=\"29005\" data=\"KLMNOPQRS\" action=\"read\">", 
+        //    "<service=\"30006\" data=\"TUVWXYZAB\" action=\"write\">", 
+        //    "<service=\"29005\" data=\"CDEFGHIJK\" action=\"read\">", 
+        //    "<service=\"30006\" data=\"LMNOPQRST\" action=\"write\">"
+        //};
+        //input = new List<string>()
+        //{
+        //    "<service=\"34010\" data=\"ABCDEFGHI\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"JKLMNOPQR\" action=\"write\">", 
+        //    "<service=\"34010\" data=''STUVWXYZA\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"BCDEFGHIJ\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"KLMNOPQRS\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"TUVWXYZAB\" action=”read\">", 
+        //    "<service=\"34010\" data=\"CDEFGHIJK\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"LMNOPQRST\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"UVWXYZABC\" action=\"write\">", 
+        //    "<service=\"34010\" data-'XYZABCDEF\" action=\"read\">", 
+        //    "<service=\"34010\" data=\"YZABCDEFG\" action=\"write\">", 
+        //    "<service=\"34010\" data=\"ZABCDEFGH\" action=\"write\">"
+        //};
+        //input = new List<string>()
+        //{
+        //    "<service=\"41017\" data=\"ABCDEFGHI\" action=\"write\">",
+        //    "<service=\"42018\" data=\"UKLMNOPQR\" action=\"read\"> ",
+        //    "<service=\"41017\" data=\"STUVWXYZA\" action=\"write\">",
+        //    "<service=\"42018\" data=\"BCDEFGHIJ\" action=\"read\">",
+        //    "<service=\"41017\" data=\"KLMNOPQRS\" action=\"write\">",
+        //    "<service=\"42018\" data=\"TUVWXYZAB\" action=\"read\">",
+        //    "<service=\"41017\" data=\"CDEFGHIJK\" action=\"write\">",
+        //    "<service=\"42018\" data=\"LMNOPQRST\" action=\"read\"> ",
+        //    "<service=\"41017\" data=\"UVWXYZABC\" action=\"write\">",
+        //    "<service=\"42018\" data=\"XYZABCDEF\" action=\"read\">",
+        //    "<service=\"41017\" data=\"YZABCDEFG\" action=\"write\">",
+        //    "<service=\"42018\" data=\"ZABCDEFGH\" action=\"read\">",
+        //    "<service=\"41017\" data=\"NEWDATAID\" action=\"write\">",
+        //    "<service=\"41019\" data=\"NEWDATAID\" action=\"write\">",
+        //    "<service=\"41020\" data=\"NEWDATAID\" action=\"write\">",
+        //    "<service=\"41021\" data=\"NEWDATAID\" action=\"write\">",
+        //    "<service=\"41022\" data=\"NEWDATAID\" action=\"write\">",
+        //    "<service=\"42018\" data=\"ANOTHERID\" action=\"write\">",
+        //    "<service=\"41019\" data=\"NEWDATAID\" action=\"read\">",
+        //    "<service=\"41019\" data=\"NEWDATAID\" action=\"read\">"
+        //};
         //input = new List<string>()
         //{
         //    "<service=\"41017\" data=\"ABCDEFGHI12 \" action=\"write\">",
